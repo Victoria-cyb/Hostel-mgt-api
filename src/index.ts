f81d5d33bfa909fed06468eb889612321726bf34
Kingdavid Oshin <67398368+mhixtadavid@users.noreply.github.com>
2025-10-21 05:16:27 +0100

    fix: add .js extension for setup import

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import cors from "cors";
-import apolloServer from "./graphql/setup";
+import apolloServer from "./graphql/setup.js";
 import { expressMiddleware } from "@as-integrations/express5"; 
 import { getToken, decodeToken } from "./utils/token";
 
